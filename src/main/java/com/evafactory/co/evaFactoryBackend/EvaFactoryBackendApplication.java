b44e50073e290dfd30acb478f2a9529158592003
medabde <medabde9@gmail.com>
2019-06-29 15:31:31 +0100

    first commit

---
+++ src/main/java/com/evafactory/co/evaFactoryBackend/EvaFactoryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.evafactory.co.evaFactoryBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaFactoryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaFactoryBackendApplication.class, args);
+	}
+
+}
